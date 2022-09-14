35a8230203d0397b93c8dc78d226ac3298437dd1
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-14 13:13:35 +0500

    add Even.php

--- src/Even.php
+++ src/Even.php
@@ -5,10 +5,40 @@ namespace BrainEven\Even;
 use function cli\line;
 use function cli\prompt;
 
-function checkEven()
+function getGreeting()
 {
     line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
     line("Hello, %s", $name);
     line('Answer "yes" if the number is even, otherwise answer "no".');
 }
+
+function checkEven()
+{
+    $correctAnswerCount = 0;
+
+
+    while($correctAnswerCount < 3) {
+        
+        $randomNumber = rand(1, 100);
+        $numberEven = $randomNumber % 2 === 0;
+
+        line("Question: " . $randomNumber);
+        $userResponse = prompt("You answer");
+        line($userResponse);
+
+        $randomNumber % 2 === 0 ? $eee = "yes" : $eee = "no";
+        if ($eee === $userResponse) {
+            line("Correct!");
+            $correctAnswerCount++;
+        } else {
+            line("'yes' is wrong answer ;(. Correct answer was 'no'.");
+            line("Let's try again, Bill!");
+            break;
+        }
+    } 
+
+    line("Congratulations, Bill!");
+
+
+} 
