d129417bdae366a3483341cd9ae9fc741891bb0c
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-15 23:42:24 +0500

    add Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -11,11 +11,13 @@ use function BrainGames\Engine\loser;
 
 function checkEven()
 {
-    line("Welcome to the Brain Games!");
+    
+    $name = getGreeting();
+    
+    /*line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
     line("Hello, %s", $name);
-    line('Answer "yes" if the number is even, otherwise answer "no".');
-
+    line('Answer "yes" if the number is even, otherwise answer "no".');*/
     $correctAnswerCount = 0;
 
     while($correctAnswerCount < 3) {
@@ -25,19 +27,19 @@ function checkEven()
         line("Question: " . $randomNumber);
         $userResponse = prompt("You answer");
 
-        $randomNumber % 2 === 0 ? $even = "yes" : $even= "no";
-        if ($even === $userResponse) {
+        $randomNumber % 2 === 0 ? $correctAnswer = "yes" : $correctAnswer= "no";
+        if ($correctAnswer=== $userResponse) {
             line("Correct!");
             $correctAnswerCount++;
         } else {
-            line("'{$userResponse}' is wrong answer ;(. Correct answer was '{$even}'");
-            line("Let's try again, %s", $name);
+            line("'{$userResponse}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+            loser($name);
             break;
         }
     } 
 
     if ($correctAnswerCount === 3) {
-        line("Congratulations, %s", $name);
+        winner($name);
     }
 
 } 
