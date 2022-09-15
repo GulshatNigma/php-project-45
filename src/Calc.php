6aec2094ddb0f33f0a350b03690a41a513c1d57f
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-15 13:14:42 +0500

    add Calc.php

--- src/Calc.php
+++ src/Calc.php
@@ -11,4 +11,59 @@ function calculator()
     $name = prompt("May I have your name?");
     line("Hello, %s", $name);
     line("What is the result of the expression?");
+
+    $correctAnswerCount = 0;
+
+    while($correctAnswerCount < 3) {
+        
+        $randomNumber1 = rand(1, 25);
+        $randomNumber2 = rand(1, 10);
+        $arrray = ["+", "-", "*"];
+        $arrrayIndex = array_rand($arrray);
+        $mathematicalOperations = $arrray[$arrrayIndex];
+
+        line("Question: " . $randomNumber1 . " " . $mathematicalOperations . " " . $randomNumber2);
+        $userResponse = prompt("You answer");
+
+            if ($mathematicalOperations === "+") {
+                $result = $randomNumber1 + $randomNumber2;
+                if ($result == $userResponse) {
+                    line("Correct!");
+                    $correctAnswerCount++;
+                } else {
+                    echo("'" . $userResponse . "' is wrong answer ;(. Correct answer was '" . $result . "'" . "\n");
+                    line("Let's try again, %s", $name);
+                    break;
+                }
+            }
+
+            if ($mathematicalOperations === "-") {
+                $result = $randomNumber1 - $randomNumber2;
+                if ($result == $userResponse) {
+                    line("Correct!");
+                    $correctAnswerCount++;
+                } else {
+                    echo("'" . $userResponse . "' is wrong answer ;(. Correct answer was '" . $result . "'" . "\n");
+                    line("Let's try again, %s", $name);
+                    break;
+                }
+            }
+
+            if ($mathematicalOperations === "*") {
+                $result = $randomNumber1 * $randomNumber2;
+                if ($result == $userResponse) {
+                    line("Correct!");
+                    $correctAnswerCount++;
+                } else {
+                    echo("'" . $userResponse . "' is wrong answer ;(. Correct answer was '" . $result . "'" . "\n");
+                    line("Let's try again, %s", $name);
+                    break;
+                }
+            }
+
+        if ($correctAnswerCount === 3) {
+            line("Congratulations, %s", $name);
+        }
+    }
+
 }
