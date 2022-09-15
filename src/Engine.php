25549487b9c88ce56eb8aed75dcb8da8199df3ca
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-15 23:42:51 +0500

    add Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -1,1 +1,24 @@
-// для общей логики игр
+<?php
+
+namespace BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function getGreeting() : string
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt("May I have your name?");
+    line("Hello, %s", $name);
+    return $name;
+}
+
+function winner(string $name)
+{
+    line("Congratulations, %s", $name);
+}
+
+function loser(string $name)
+{
+    line("Let's try again, {$name}");
+}
