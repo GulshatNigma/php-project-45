25f7822c242f2f9c87bed8a13078ac33b0af2231
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-16 22:19:46 +0500

    add Gcd.php

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -11,15 +11,29 @@ use function BrainGames\Engine\correctAnswer;
 function gcd()
 {
     $gameConditions = "Find the greatest common divisor of given numbers.";
-    $name = getGreeting($gameConditions);
+    $name = getGreeting($gameConditions); 
 
-    $randomNumber1 = rand(1, 25);
-    $randomNumber2 = rand(1, 10);
-    $question = "{$randomNumber1} {$randomNumber2}";
-    $userResponse = question($question); 
+    $correctAnswerCount = 0;
+    
+    while($correctAnswerCount < 3) {
+        
+        $randomNumber1 = rand(1, 25);
+        $randomNumber2 = rand(1, 10);
+        $question = "{$randomNumber1} {$randomNumber2}";
+        $userResponse = question($question); 
+        
+        
+        $maxNumber = ($randomNumber1 > $randomNumber2) ? $randomNumber1 : $randomNumber2;
+        $minNumber = ($randomNumber1 < $randomNumber2) ? $randomNumber1 : $randomNumber2;
+        if ($randomNumber1 === $randomNumber2) {
+            $correctAnswer = $randomNumber1;
+        } elseif ($randomNumber1 > $randomNumber2) {
 
+        }
 
+        $correctAnswer == $userResponse ? correctAnswer($correctAnswerCount) : playerLosing($name, $userResponse, $correctAnswer);
 
-
-
+        
+    }     
+    playerWinner($name, $correctAnswerCount);
 }
