74e7888b3df42a0f27d8afa769d14854120303d3
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-16 19:52:47 +0500

    add Gcd.php

---
+++ src/Games/Gcd.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BrainEven\Gcd;
+
+use function BrainGames\Engine\getGreeting;
+use function BrainGames\Engine\playerWinner;
+use function BrainGames\Engine\playerLosing;
+use function BrainGames\Engine\question;
+use function BrainGames\Engine\correctAnswer;
+
+function gcd()
+{
+    $gameConditions = "Find the greatest common divisor of given numbers.";
+    $name = getGreeting($gameConditions);
+
+    
+
+
+
+
+
+}
