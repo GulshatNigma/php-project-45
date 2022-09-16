22281966d379bcc4e266ec16f1b412d7a02a6dba
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-16 15:03:08 +0500

    update Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -8,6 +8,7 @@ use function cli\prompt;
 use function BrainGames\Engine\getGreeting;
 use function BrainGames\Engine\playerWinner;
 use function BrainGames\Engine\playerLosing;
+use function BrainGames\Engine\quest;
 
 function checkEven()
 {
@@ -19,17 +20,16 @@ function checkEven()
     while($correctAnswerCount < 3) {
         
         $randomNumber = rand(1, 100);
+        $quest = $randomNumber;
 
-        line("Question: " . $randomNumber);
-        $userResponse = prompt("You answer");
+        $userResponse = quest($quest);
 
         $randomNumber % 2 === 0 ? $correctAnswer = "yes" : $correctAnswer= "no";
         if ($correctAnswer=== $userResponse) {
             line("Correct!");
             $correctAnswerCount++;
         } else {
-            line("'{$userResponse}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
-            playerLosing($name);
+            playerLosing($name, $userResponse);
             break;
         }
     } 
