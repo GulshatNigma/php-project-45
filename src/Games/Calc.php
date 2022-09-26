0a9239620a9a240eb1339fbe3bd876edabe19923
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-26 14:23:48 +0500

    update brain-calc

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -1,25 +1,20 @@
 <?php
 
-namespace BrainGames\Calc;
+namespace BrainGames\Src\Games\Calc;
 
 use function BrainGames\Engine\getGreeting;
-use function BrainGames\Engine\isPlayerWinner;
-use function BrainGames\Engine\getUserAnswer;
-use function BrainGames\Engine\checkUserAnswer;
+
 
 function calculatorRun()
 {
     $gameRule = "What is the result of the expression?";
-    $name = getGreeting($gameRule);
-    $correctAnswerCount = 0;
 
-    while ($correctAnswerCount < 3) {
         $randomNumber1 = rand(1, 25);
         $randomNumber2 = rand(1, 10);
         $arrray = ["+", "-", "*"];
         $mathematicalOperations = $arrray[array_rand($arrray)];
+        
         $question = ("{$randomNumber1} {$mathematicalOperations} {$randomNumber2}");
-        $userAnser = getUserAnswer($question);
 
         switch ($mathematicalOperations) {
             case "+":
@@ -32,9 +27,5 @@ function calculatorRun()
                 $correctAnswer = $randomNumber1 * $randomNumber2;
                 break;
         }
-        if (checkUserAnswer((string) $correctAnswer, $userAnser, $correctAnswerCount, $name) === false) {
-            break;
-        }
+        getGreeting($gameRule, $correctAnswer, $question);
     }
-    isPlayerWinner($correctAnswerCount, $name);
-}
