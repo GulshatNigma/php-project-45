8f71c3a152b303149062415b4635d7a7d283ec5c
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-27 01:22:06 +0500

    update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -4,9 +4,8 @@ namespace BrainGames\Engine;
 
 use function cli\line;
 use function cli\prompt;
-use function BrainGames\Src\Games\Even\getQuestion;
 
-function getGreeting(string $gameRule, $correctAnswer)
+function getGreeting(string $gameRule, $correctAnswer, $question): bool
 {
     line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
@@ -15,22 +14,28 @@ function getGreeting(string $gameRule, $correctAnswer)
 
     $correctAnswerCount = 0;
     $gameScore = 3;
-    while ($correctAnswerCount < 3) {
-        $question = getQuestion($correctAnswer);
+
+    while ($correctAnswerCount < $gameScore) {
+
         line("Question: {$question}");
         $userAnswer = prompt("You answer");
         
         if ($correctAnswer == $userAnswer) {
             line("Correct!");
             $correctAnswerCount++;
+            echo ($correctAnswerCount);
+            $result = true;
+            return $result;
         } else {
             line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
             line("Let's try again, {$name}!");
-            break;
-        }
+            $result = false;
+            return $result;
         }
         if ($correctAnswerCount === $gameScore) {
             line("Congratulations, {$name}!");
+            $result = false;
+            return $result;
         }
     }
-
+}
