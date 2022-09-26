8db6779ca2850749120459527d50dbfbf2463921
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-26 14:15:50 +0500

    даже не знаю как назвать. update Engine.php && Even.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,38 +5,35 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function getGreeting(string $gameRule): string
+function getGreeting(string $gameRule, $correctAnswer, $question): string
 {
     line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
     line("Hello, %s", $name);
     line("{$gameRule}");
-    return $name;
-}
 
-function getUserAnswer(string &$question): string
-{
-    line("Question: {$question}");
-    $userAnser = prompt("You answer");
-    return $userAnser;
-}
+    $gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-function checkUserAnswer(string $correctAnswer, string $userAnswer, int &$correctAnswerCount, string $name)
-{
-    if ($correctAnswer == $userAnswer) {
-        line("Correct!");
-        return $correctAnswerCount++;
-    } else {
-        line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
-        line("Let's try again, {$name}!");
-        return false;
-    }
-}
-
-function isPlayerWinner(int $correctAnswerCount, string $name)
-{
+    $correctAnswerCount = 0;
     $gameScore = 3;
-    if ($correctAnswerCount === $gameScore) {
-        line("Congratulations, {$name}!");
+
+    while ($correctAnswerCount < 3) {
+        line("Question: {$question}");
+        $userAnswer = prompt("You answer");
+        
+        if ($correctAnswer == $userAnswer) {
+            line("Correct!");
+            return $correctAnswerCount++;
+        } else {
+            line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+            line("Let's try again, {$name}!");
+            return false;
+
+            if ($correctAnswerCount === $gameScore) {
+                line("Congratulations, {$name}!");
+            }
+        }
+
+
     }
 }
--- src/Games/Even.php
+++ src/Games/Even.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BrainGames\Even;
+namespace BrainGames\Src\Games\Even;
 
 use function BrainGames\Engine\getGreeting;
 use function BrainGames\Engine\isPlayerWinner;
@@ -11,18 +11,7 @@ function checkEvenRun()
 {
     $correctAnswer = "";
     $gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
-    $name = getGreeting($gameRule);
-
-    $correctAnswerCount = 0;
-
-    while ($correctAnswerCount < 3) {
         $question = rand(1, 100);
-        $userAnswer = getUserAnswer($question);
-
         $question % 2 === 0 ? $correctAnswer = "yes" : $correctAnswer = "no";
-        if (checkUserAnswer($correctAnswer, $userAnswer, $correctAnswerCount, $name) === false) {
-            break;
-        }
-    }
-    isPlayerWinner($correctAnswerCount, $name);
+        getGreeting($gameRule, $correctAnswer, $question);
 }
