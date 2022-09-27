9750c6dfc269f107e73c063f6fb0880c59676041
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-27 23:30:06 +0500

    update Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -7,14 +7,13 @@ use function BrainGames\Engine\getGreeting;
 function calculatorRun()
 {
     $gameRule = "What is the result of the expression?";
-    $correctAnswer = 0;
 
-    function getQuestion(&$correctAnswer) {
+    for ($i = 0; $i < 3; $i++) {
         $randomNumber1 = rand(1, 25);
         $randomNumber2 = rand(1, 10);
         $arrray = ["+", "-", "*"];
         $mathematicalOperations = $arrray[array_rand($arrray)];
-        
+
         $question = ("{$randomNumber1} {$mathematicalOperations} {$randomNumber2}");
 
         switch ($mathematicalOperations) {
@@ -28,8 +27,8 @@ function calculatorRun()
                 $correctAnswer = $randomNumber1 * $randomNumber2;
                 break;
         }
-        return $question;
-        }
-
-        getGreeting($gameRule, $correctAnswer);
+        $questionArray[] = $question;
+        $correctAnswerArray[] = $correctAnswer;
     }
+    getGreeting($gameRule, $questionArray, $correctAnswerArray);
+}
