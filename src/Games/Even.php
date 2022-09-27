b799c83fb8a22b268598c8b70f15ccf36d147eed
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-27 14:08:39 +0500

    update brain-even

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -3,14 +3,17 @@
 namespace BrainGames\Src\Games\Even;
 
 use function BrainGames\Engine\getGreeting;
+use function BrainGames\Engine\getUserAnswer;
+use function BrainGames\Engine\isPlayerWinner;
+use function BrainGames\Engine\checkUserAnswer;
 
-
-function checkEvenRun($result = true)
+function checkEvenRun()
 {
+
     $gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
-    
 
         $question = rand(1, 100);
         $question % 2 === 0 ? $correctAnswer = "yes" : $correctAnswer = "no";
-        $result = getGreeting($gameRule, $correctAnswer, $question);
+
+        getGreeting($gameRule, $correctAnswer, $question);
 }
