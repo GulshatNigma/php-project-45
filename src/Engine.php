8ea4b4a8c1428f70813bd6301df25e8c15b125b0
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-09-28 14:21:05 +0500

    update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -19,32 +19,19 @@ function gameRun(string $gameRule, array $questionArray, array $correctAnswerArr
         $question = $questionArray[$correctAnswerCount];
         $correctAnswer = $correctAnswerArray[$correctAnswerCount];
 
-        $userAnswer = getUserAnswer($question);
+        line("Question: {$question}");
+        $userAnswer = prompt("You answer");
 
-        if (checkUserAnswer($correctAnswer, $userAnswer, $correctAnswerCount, $name) === false) {
+        if ($correctAnswer == $userAnswer) {
+            line("Correct!");
+            $correctAnswerCount++;
+        } else {
+            line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+            line("Let's try again, {$name}!");
             break;
         }
     }
     if ($correctAnswerCount === $gameScore) {
         line("Congratulations, {$name}!");
-    }
-}
-
-function getUserAnswer(string &$question): string
-{
-    line("Question: {$question}");
-    $userAnswer = prompt("You answer");
-    return $userAnswer;
-}
-
-function checkUserAnswer(string $correctAnswer, string $userAnswer, int &$correctAnswerCount, string $name)
-{
-    if ($correctAnswer == $userAnswer) {
-        line("Correct!");
-        return $correctAnswerCount++;
-    } else {
-        line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
-        line("Let's try again, {$name}!");
-        return false;
     }
 }
